559d8a94d283f89ecf70bb86bc67b57bcb0f7e20
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2024-04-17 00:05:49 -0300

    Do You Think God Stays in Heaven Because He too Lives in Fear of What He's Created

--- QUE GIS.py
+++ QUE GIS.py
@@ -1,5 +1,6 @@
 import os 
 from os import walk
+import processing
 
 from qgis.core import (QgsVectorLayer, QgsRasterLayer)
 
@@ -12,10 +13,43 @@ def load_raster(path, name = "LAYER"):
         print("Path: " + src_path + "/" + path)
     else:
         QgsProject.instance().addMapLayer(rlayer)
+        return rlayer
+    
+def raster_equation_onearg(input_path):
+    input_raster = rasters[4]
+    output_raster = src_path + "/TESTE"
+    parameters = {
+        'INPUT_A' : input_path,
+        'BAND_A' : 1,
+        'FORMULA' : '2',
+        'OUTPUT' : output_raster}
+        
+    processing.runAndLoadResults('gdal:rastercalculator', parameters)
+
 
 filenames = next(walk(src_path), (None, None, []))[2]
+
+rasters = []
+paths = []
+i = 0
+
 for filen in filenames:
-    load_raster(filen, name = filen)
+    rasters.append(load_raster(filen, name = filen))
+    paths.append(filen)
+    i+=1
+    pass
+
+exp = QgsExpression('1 + 1 = 2')
+exp.evaluate()
 
 #load_raster("/LC08_L1TP_219076_20231218_20240103_02_T1_B4.TIF")
+
+#expression1 = QgsExpression('LC08_L1TP_219076_20231218_20240103_02_T1_B4/3')
+
+root = QgsProject.instance().layerTreeRoot()
+
+node = root.addGroup("RAW")
+node.insertChildNode(0,rasters[2])
+
+#g.removeChildNode(h)
 
