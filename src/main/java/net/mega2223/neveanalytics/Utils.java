8a76743e53dc75ffdedd9338ef30bef0a75c9990
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2024-04-26 17:21:29 -0300

    Utils.java was not at the git repo for some reason

---
+++ src/main/java/net/mega2223/neveanalytics/Utils.java
@@ -0,0 +1,16 @@
+package net.mega2223.neveanalytics;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Utils {
+    public static void saveFile(String data, String path) throws IOException {
+        File file = new File(path);
+        if(!file.exists()){file.createNewFile();}
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file,false));
+        writer.write(data);
+        writer.close();
+    }
+}
