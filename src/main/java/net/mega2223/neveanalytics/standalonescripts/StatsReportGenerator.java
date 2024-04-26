241c3a79cb443a47cb890021f96e024b4aa7a8ce
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2024-04-26 16:20:30 -0300

    Configs.json 2

--- src/main/java/net/mega2223/neveanalytics/standalonescripts/StatsReportGenerator.java
+++ src/main/java/net/mega2223/neveanalytics/standalonescripts/StatsReportGenerator.java
@@ -3,8 +3,10 @@ package net.mega2223.neveanalytics.standalonescripts;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import mil.nga.tiff.Rasters;
 import net.mega2223.neveanalytics.Constants;
+import net.mega2223.neveanalytics.Utils;
 import net.mega2223.neveanalytics.objects.LandsatImage;
 
 import java.io.File;
@@ -13,6 +15,7 @@ import java.io.IOException;
 
 public class StatsReportGenerator {
     public static float SNOW_THRESHOLD = .5F;
+
     public static void main(String[] args) throws IOException {
         int snowPixels;
         File folder = new File(Constants.DATA_PATH+"\\NDSI");
@@ -36,13 +39,19 @@ public class StatsReportGenerator {
                 }
             }
             imgAct.discardBuffer();
-            System.out.println("SNOW PIXELS FOR "+ files[i].getName() +": " + snowPixels + " (Threshold "+SNOW_THRESHOLD+")");
+            printAndBuffer("SNOW PIXELS FOR "+ files[i].getName() +": " + snowPixels + " (Threshold "+SNOW_THRESHOLD+")");
             float x = geoTransform.get(1).getAsFloat(), y = geoTransform.get(5).getAsFloat();
             x = Math.abs(x); y = Math.abs(y);
-            System.out.println("PIXEL SIZE: (" + x + "," + y + ")");
-            System.out.println("TOTAL AREA COVERED: " + snowPixels * Math.sqrt(x * y) + ", " + 100*((float)snowPixels)/(raster.getNumPixels()) + "%");
-            System.out.println("------------------------------------------------------------");
+            printAndBuffer("PIXEL SIZE: (" + x + "," + y + ")");
+            printAndBuffer("TOTAL AREA COVERED: " + snowPixels * Math.sqrt(x * y) + ", " + 100*((float)snowPixels)/(raster.getNumPixels()) + "%");
+            printAndBuffer("------------------------------------------------------------");
         }
-
+        Utils.saveFile(log.toString(),Constants.DATA_PATH+"\\Coverage.txt");
+        log.delete(0,log.length());
+    }
+    protected static StringBuilder log = new StringBuilder();
+    static void printAndBuffer(String data){
+        System.out.println(data);
+        log.append(data).append("\n");
     }
 }
